c2115cd2c1a8621fc66a9a51f3f56193f1eb801e
Rob Reynolds <ferventcoder@gmail.com>
2015-10-12 14:51:49 -0500

    (infra) add solution

---
+++ src/sudo/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2015 - Present RealDimensions Software, LLC
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("17299ac3-6a7a-4a21-955c-daff82867edc")]
